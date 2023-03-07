9bc48b44b8d41a85727c10ebdaf4f0c594301492
Mritunjay Upadhyay <mupadhyay00@gmail.com>
2023-03-08 02:41:43 +0700

    feat: Added styled component

---
+++ src/styles/theme/index.ts
@@ -0,0 +1,9 @@
+export const theme = {
+    lightBackground: "#eeeed2",
+    darkBackground: "#769656",
+    primaryColor: "#444",
+    fontFamily: "'Arvo', serif",
+    desktop: "960px",
+    tablet: "769px",
+    mobile: "500px"
+}
