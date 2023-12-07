b1b8f717213973400d50482b0f8ee10f82c7b3c2
Mritunjay <mupadhyay00@gmail.com>
2023-12-07 21:47:05 +0700

    🚧 chore(App.tsx): add TODO comment to remind adding localstorage for saving game state

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.scss';
 import { GamePage } from './pages/game_page/game_page';
 import { theme } from './styles/theme';
 
+// TODO: Add localstorage for saving game state
 function App() {
   return (
     <ThemeProvider theme={theme}>
